7a5da3a24b17079576c1323d3de18ef24958b6bd
myracheng <myracheng027@gmail.com>
2024-01-30 11:01:32 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,14 +13,14 @@ setup(
     
     license='BSD 2-clause',
     install_requires=['regex',
-                      'spacy==3.4.4',
+                      'spacy==3.7.2',
                       'numpy',
                       'scipy',
                       'transformers',
                       'pandas',
                       'torch',
-                      'en-core-web-sm @ https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.4.1/en_core_web_sm-3.4.1-py3-none-any.whl'
-                      ],
+                      'en-core-web-sm @ https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.7.1/en_core_web_sm-3.7.1-py3-none-any.whl#sha256=86cc141f63942d4b2c5fcee06630fd6f904788d2f0ab005cce45aadb8fb73889'
+                     ],
 
     classifiers=[     
         'Programming Language :: Python :: 3.11',
