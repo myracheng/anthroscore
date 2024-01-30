b891433a4a4fee23394855eb78896709092476d4
Myra Cheng <myracheng027@gmail.com>
2024-01-30 12:59:12 -0800

    change version

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from setuptools import setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='anthroscore-eacl',
-    __version__ = "0.0.0",
+    __version__ = "0.0.1",
 
 
     url='https://github.com/myracheng/anthroscore',
     author='Myra Cheng',
     author_email='myra@cs.stanford.edu',
-
+description = "Package to compute AnthroScore, a computational linguistic measure of anthropomorphism in text",
     packages=['src'],
-    
     license='BSD 2-clause',
     install_requires=['regex',
                       'spacy==3.7.2',
@@ -20,7 +22,8 @@ setup(
                       'pandas',
                       'torch',
                      ],
-
+	long_description=long_description,
+    long_description_content_type='text/markdown',
     classifiers=[     
         'Programming Language :: Python :: 3.11',
     ],
